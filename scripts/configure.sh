a2294e2d782b655bf9a4cd9071433067bb918ce5
Dennis ten Hoove <dennistenhoove@protonmail.com>
2023-05-31 21:20:26 +0200

    Add custom sysctl tunables

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -54,6 +54,9 @@ task_wrapper sudo arch-chroot $workdir locale-gen
 # Copy Systemd-boot configuration
 task_wrapper sudo cp -rv $osidir/bits/systemd-boot/* $workdir/boot/loader/
 
+# Set custom sysctl tunables
+task_wrapper sudo install -m600 $osdir/bits/99-sysctl.conf $workdir/etc/sysctl.d/
+
 # Add dconf tweaks for GNOME desktop configuration
 task_wrapper sudo cp -rv $osidir/bits/dconf $workdir/etc/
 task_wrapper sudo arch-chroot $workdir dconf update
